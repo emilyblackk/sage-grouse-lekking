e58a63a633f877301b79b5cae793d917c92cc132
Emily Black <black.emily.nicole@gmail.com>
2023-06-01 14:03:33 -0600

    Basic data cleaning example of floppy disk data

---
+++ scripts/basic_data_cleaning_example.R
@@ -0,0 +1,22 @@
+#Emily Black
+#Example data cleaning - floppy disk
+#Created: 1 June 2023
+
+#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_
+#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_
+
+#Part 0. Script setup
+#clear R's brain
+rm(list=ls())
+
+#load relevant libraries for script
+pkgs <- c("tidyverse")
+#install.packages(pkgs)
+lapply(pkgs, library, character.only = TRUE)
+rm(pkgs)
+
+#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_
+#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_
+
+#Part 1: Opening the data
+
